b60ea0c562ba3911b728c05f52274d06b8736316
seongwonlee <jusk2@naver.com>
2017-08-18 14:08:39 +0900

    code clean

--- Booth/app.js
+++ Booth/app.js
@@ -1,63 +1,64 @@
 const SerialPort = require('serialport');
-const file = '/dev/ttyACM0';
-const file2 = '/dev/ttyACM1'; 
 const GPS = require('gps');
+const net = reauire('net');
+const port1 = '/dev/ttyACM0';
+const port2 = '/dev/ttyACM1';
 const gps = new GPS;
-
-const port = new SerialPort(file, {
-	buaudrate:9600,
-	parser: SerialPort.parsers.readline('\r\n')
-});
-
-const port2 = new SerialPort(file2, {
-	baudrate: 4800, 
-	parser: SerialPort.parsers.readline('\r\n')
-}); 
 
 let latitude = 0;
 let longitude = 0;
-let seSensigData = '';
 let seTmpData = '';
 let boothSensor = '';
 
-port2.on('data', function(data) {
-		gps.update(data); 
-}); 
-
-gps.on('data', function(data) {
-			
-	if (gps.state.lat !== undefined && gps.state.lon !== undefined) {
-				latitude = gps.state.lat.toString(); 
-	            latitude = latitude.substring(0,7);
-
-				longitude = gps.state.lon.toString(); 
-				longitude = longitude.substring(0,9);
-	}
+const serialArduino = new SerialPort(port1, {
+    buaudrate: 9600,
+    parser: SerialPort.parsers.readline('\r\n')
 });
 
-port.on('data', function(data) {
-	const mbRex = new Buffer(data);
-	const string = mbRex.toString('ascii').trim();
-	if(string.indexOf('{') > -1 || seTmpData.length > 0) {
-		seTmpData = seTmpData + string;
-		const indexFirst = seTmpData.indexOf('{');
-		const indexLast = seTmpData.indexOf('}');
-		if(indexFirst !== -1 && indexLast !== -1) {
-			seSensingData = seTmpData.substr(indexFirst, indexLast + 1);
-			seTmpData = "";
-
-			let re = /\0/g; 
-			let str = seSensingData.replace(re, ""); 
-			let msg = JSON.parse(str); 
-
-			boothSensor = {trash: msg.trash, smoke: msg.smoke, lat: latitude, lon: longitude};
-			JSON.stringify(boothSensor); 
-		}
-	}
+const serialGPS = new SerialPort(port2, {
+    baudrate: 4800,
+    parser: SerialPort.parsers.readline('\r\n')
 });
 
-setInterval(function() { 
-	console.log(boothSensor);
-	console.log(boothSensor.lat);
-},1000);
+serialGPS.on('data', function (data) {
+    gps.update(data);
+});
+
+gps.on(, function () {
+
+    if (gps.state.lat !== undefined && gps.state.lon !== undefined) {
+        latitude = gps.state.lat.toString();
+        latitude = latitude.substring(0, 7);
+
+        longitude = gps.state.lon.toString();
+        longitude = longitude.substring(0, 9);
+    }
+});
+
+serialArduino.on('data', function (data) {
+
+    //TODO Arduino JSON format
+    const mbRex = new Buffer(data);
+    const string = mbRex.toString('ascii').trim();
+    if (string.indexOf('{') > -1 || seTmpData.length > 0) {
+        seTmpData = seTmpData + string;
+        const indexFirst = seTmpData.indexOf('{');
+        const indexLast = seTmpData.indexOf('}');
+        if (indexFirst !== -1 && indexLast !== -1) {
+            let seSensingData = seTmpData.substr(indexFirst, indexLast + 1);
+            seTmpData = '';
+
+            let re = /\0/g;
+            let str = seSensingData.replace(re, "");
+            let msg = JSON.parse(str);
+            boothSensor = {trash: msg.trash, smoke: msg.smoke, lat: latitude, lon: longitude};
+            JSON.stringify(boothSensor);
+        }
+    }
+});
+
+setInterval(function () {
+    console.log(boothSensor);
+    console.log(boothSensor.lat);
+}, 1000);
 
--- Kiosk/app.js
+++ Kiosk/app.js
@@ -3,7 +3,7 @@ const file = '/dev/ttyACM0';
 const Readline = SerialPort.parsers.Readline;
 
 const port = new SerialPort(file, {
-	baudrate: 9600
+    baudrate: 9600
 });
 
 const parser = port.pipe(new Readline({delimeter: '\r\n'}));
@@ -11,21 +11,19 @@ const parser = port.pipe(new Readline({delimeter: '\r\n'}));
 let seTmpData = '';
 let seSensingData = '';
 let msg = '';
-parser.on('data', function(data) {
+parser.on('data', function (data) {
 
-	//TODO Arduino json format
+    //TODO Arduino json format
     const mbRex = new Buffer(data);
-    const string  = mbRex.toString('ascii') .trim();
-	if(string.indexOf('{') > -1 || seTmpData.length > 0) {
-		seTmpData = seTmpData + string;
+    const string = mbRex.toString('ascii').trim();
+    if (string.indexOf('{') > -1 || seTmpData.length > 0) {
+        seTmpData = seTmpData + string;
         const indexFirst = seTmpData.indexOf('{');
         const indexLast = seTmpData.indexOf('}');
-		if(indexFirst !== -1 && indexLast !== -1) {
-			seSensingData =seTmpData.substr(indexFirst, indexLast+1);
-			//console.log(seSensingData);
-			seTmpData = "";
-			msg = JSON.parse(seSensingData);
+        if (indexFirst !== -1 && indexLast !== -1) {
+            seSensingData = seTmpData.substr(indexFirst, indexLast + 1);
+            seTmpData = "";
+            msg = JSON.parse(seSensingData);
         }
-		console.log(msg.sensing);
-	}
+    }
 });
