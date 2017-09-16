4bcc781fae4270b4f3e436bd235de7c86276449f
seongwonlee <swlee@wisoft.io>
2017-09-16 16:27:34 +0900

    부스센서 코드 정리

--- Booth/app.js
+++ Booth/app.js
@@ -1,9 +1,12 @@
+'use strict';
+
 const SerialPort = require('serialport');
 const GPS = require('gps');
-const net = require('net');
 const port1 = '/dev/ttyACM0';
 const port2 = '/dev/ttyACM1';
 const gps = new GPS;
+const tcp = require('./tcp');
+tcp.getConnection();
 
 let latitude = 0;
 let longitude = 0;
@@ -39,10 +42,12 @@ serialArduino.on('data', function (data) {
 
     const mbRex = new Buffer(data);
     const string = mbRex.toString('ascii').trim();
+
     if (string.indexOf('{') > -1 || seTmpData.length > 0) {
         seTmpData = seTmpData + string;
         const indexFirst = seTmpData.indexOf('{');
         const indexLast = seTmpData.indexOf('}');
+
         if (indexFirst !== -1 && indexLast !== -1) {
             let seSensingData = seTmpData.substr(indexFirst, indexLast + 1);
             seTmpData = '';
@@ -50,51 +55,9 @@ serialArduino.on('data', function (data) {
             let re = /\0/g;
             let str = seSensingData.replace(re, "");
             let msg = JSON.parse(str);
-			sensor = {code: 'booth', trash: msg.trash, smoke: msg.smoke, lat: latitude, lon: longitude};
+            sensor = {code: 'booth', trash: msg.trash, smoke: msg.smoke, lat: latitude, lon: longitude};
             JSON.stringify(sensor);
+            tcp.writeData(sensor);
         }
     }
 });
-
-function getConnection(connName) {
-    let client = net.connect({port: 5001, host: 'jusk2.asuscomm.com'}, function () {
-        console.log(connName + ' Connected: ');
-        console.log('   local = %s:%s', this.localAddress, this.localPort);
-        console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
-        this.setEncoding('utf-8');
-		register(client); 
-
-        this.on('data', function (data) {
-            console.log(connName + ' From Server: ' + data);
-        });
-        this.on('end', function () {
-            console.log(connName + ' Client disconnected');
-        });
-        this.on('error', function (err) {
-            console.log('Socket Error: ', JSON.stringify(err));
-        });
-        this.on('timeout', function () {
-            console.log('Socket Timed Out');
-        });
-        this.on('close', function () {
-            console.log('Socket Closed');
-        });
-    });
-    return client;
-}
-
-function register(socket) {
-	let serviceRegister = {code: 'register', service: 'booth'}; 
-	writeData(socket, serviceRegister); 
-}
-
-function writeData(socket, data) {
-    socket.write(JSON.stringify(data));
-}
-const node1 = getConnection('node1');
-
-setInterval(function () {
-    console.log(sensor);
-    writeData(node1, sensor);
-}, 2000);
-
---
+++ Booth/tcp.js
@@ -0,0 +1,39 @@
+'use strict';
+const net = require('net');
+let server = null;
+
+exports.getConnection = () => {
+    server = net.connect({port: 5001, host: 'jusk2.asuscomm.com'}, function () {
+        console.log(' Connected ');
+        console.log('   local = %s:%s', this.localAddress, this.localPort);
+        console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
+        this.setEncoding('utf-8');
+        register();
+
+        this.on('data', function (data) {
+            console.log(connName + ' From Server: ' + data);
+        });
+        this.on('end', function () {
+            console.log(connName + ' Client disconnected');
+        });
+        this.on('error', function (err) {
+            console.log('Socket Error: ', JSON.stringify(err));
+        });
+        this.on('timeout', function () {
+            console.log('Socket Timed Out');
+        });
+        this.on('close', function () {
+            console.log('Socket Closed');
+        });
+    })
+};
+
+exports.writeData = (data) => {
+    if (server !== undefined || server !== null)
+        server.write(data);
+};
+
+function register() {
+    let serviceRegister = {code: 'register', service: 'booth'};
+    server.write(JSON.stringify(serviceRegister));
+}
